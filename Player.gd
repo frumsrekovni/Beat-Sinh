06cf2b0801cfcb6a2cb3e36db5d8f4c552e99003
frumsrekovni <joshua.backstrom@gmail.com>
2023-04-23 12:03:58 +0200

    -bound to viewport -sluggish movement

--- Player.gd
+++ Player.gd
@@ -2,12 +2,19 @@ extends CharacterBody2D
 
 
 const SPEED = 1000.0
-var motion = Vector2.ZERO
-
+var screen_dimensions
+func _ready():
+	screen_dimensions = get_viewport_rect().size
+	print(screen_dimensions)
 func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_left"):
-		motion = move_and_collide(Vector2(-SPEED * delta, 0))
+		#motion = move_and_collide(Vector2(-SPEED * delta, 0))
+		velocity.x -= 1
 	if Input.is_action_pressed("ui_right"):
-		motion = move_and_collide(Vector2(SPEED * delta, 0))
+		#motion = move_and_collide(Vector2(SPEED * delta, 0))
+		velocity.x += 1
+	
+	position += velocity * delta * SPEED
+	position = position.clamp(Vector2.ZERO, screen_dimensions)
 	
